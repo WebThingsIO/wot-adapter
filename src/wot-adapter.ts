2036943ccf90a0b38e15eff299ce1716a7ed770d
reluc <relu.cri@gmail.com>
2021-04-12 18:13:25 +0200

    Add eslint and prettier

--- src/wot-adapter.ts
+++ src/wot-adapter.ts
@@ -1,30 +1,28 @@
 /**
- * WoT Adapter.ts 
+ * WoT Adapter.ts
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.*
  */
 
-import { AddonManagerProxy } from "gateway-addon/lib/addon-manager-proxy";
-import manifest from '../manifest.json'
+import { AddonManagerProxy } from 'gateway-addon/lib/addon-manager-proxy';
+import manifest from '../manifest.json';
 
 class WoTAdapter extends Adapter {
-    constructor(manager:AddonManagerProxy) {
-        super(manager,manifest.id,manifest.id);
-    }
+  constructor(manager: AddonManagerProxy) {
+    super(manager, manifest.id, manifest.id);
+  }
 
-    async loadThing(url:string,retryCounter:number) {
-         //TODO: See https://github.com/WebThingsIO/thing-url-adapter/blob/master/thing-url-adapter.js#L544
-    }
+  async loadThing(url: string, retryCounter: number) {
+    // TODO: See https://github.com/WebThingsIO/thing-url-adapter/blob/master/thing-url-adapter.js#L544
+  }
 
-    unloadThing(url:string){
-        //TODO: See https://github.com/WebThingsIO/thing-url-adapter/blob/master/thing-url-adapter.js#L635
-    }
-
+  unloadThing(url: string) {
+    // TODO: See https://github.com/WebThingsIO/thing-url-adapter/blob/master/thing-url-adapter.js#L635
+  }
 }
 
-
-export default function loadWoTAdapter(manager:AddonManagerProxy) {
-    //TODO: See https://github.com/WebThingsIO/thing-url-adapter/blob/master/thing-url-adapter.js#L844
+export default function loadWoTAdapter(manager: AddonManagerProxy) {
+  // TODO: See https://github.com/WebThingsIO/thing-url-adapter/blob/master/thing-url-adapter.js#L844
 }
