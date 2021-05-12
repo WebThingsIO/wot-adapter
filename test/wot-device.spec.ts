13ad4c7c1c92e07cfdeb61dbdf4071bf0ae7efcf
reluc <relu.cri@gmail.com>
2021-05-12 12:27:02 +0200

    Add tests for wot-device

---
+++ test/wot-device.spec.ts
@@ -0,0 +1,110 @@
+import chai, { expect } from 'chai';
+import WoTDevice from '../src/wot-device';
+import { stubInterface } from 'ts-sinon';
+import { ConsumedThing } from '@node-wot/core';
+import { WoTAdapter } from '../src/wot-adapter';
+import sinon, { fake, SinonStub, spy } from 'sinon';
+import { AddonManagerProxy, Event } from 'gateway-addon';
+import sinonChai from 'sinon-chai';
+
+chai.use(sinonChai);
+
+describe('WoT Device tests', () => {
+  let testDevice: WoTDevice;
+
+  const mockConsumedThing = stubInterface<ConsumedThing>();
+  const mockAdapter = stubInterface<WoTAdapter>();
+  const mockManager = stubInterface<AddonManagerProxy>();
+
+
+  beforeEach(() => {
+    mockAdapter.getManager.returns(mockManager);
+  });
+
+  afterEach(() => {
+    sinon.reset();
+    testDevice && testDevice.destroy();
+  });
+
+  it('Should write property', () => {
+    const td = {
+      properties: {
+        test: {
+          type: 'number',
+        },
+      },
+    };
+
+    testDevice = new WoTDevice(mockAdapter, 'test', td, mockConsumedThing);
+    testDevice.setProperty('testProperty', 1);
+    expect(mockConsumedThing.writeProperty.calledOnceWith('testProperty', 1)).true;
+    testDevice.destroy();
+  });
+
+  it('Should read property', async () => {
+    const td = {
+      properties: {
+        test: {
+          type: 'number',
+        },
+      },
+    };
+
+    testDevice = new WoTDevice(mockAdapter, 'test', td, mockConsumedThing);
+    mockConsumedThing.readProperty.returns(Promise.resolve(1));
+    const value = await testDevice.getProperty('test');
+
+    expect(value).be.eqls(1);
+    expect(mockConsumedThing.readProperty).calledOnceWith('test');
+    testDevice.destroy();
+  });
+
+  it('Should invoke an action', async () => {
+    const td = {
+      actions: {
+        test: {
+          input: {
+            type: 'number',
+          },
+        },
+      },
+    };
+
+    testDevice = new WoTDevice(mockAdapter, 'test', td, mockConsumedThing);
+    await testDevice.requestAction('1234', 'test', 1);
+
+    expect(mockConsumedThing.invokeAction).calledOnceWith('test', 1);
+    testDevice.destroy();
+  });
+
+  it('Should fire events', async () => {
+    const td = {
+      events: {
+        test: {
+          type: 'number',
+        },
+      },
+    };
+    let eventCallback: WoT.WotListener;
+    const subscribe: SinonStub<[name: string,
+      listener: WoT.WotListener,
+      options?: WoT.InteractionOptions | undefined],
+    Promise<void>> = fake((event: string, callback: WoT.WotListener) => {
+      eventCallback = callback;
+    }) as SinonStub<[name: string,
+      listener: WoT.WotListener, options?:
+      WoT.InteractionOptions | undefined], Promise < void>>;
+
+    mockConsumedThing.subscribeEvent = subscribe;
+
+    testDevice = new WoTDevice(mockAdapter, 'test', td, mockConsumedThing);
+    const eventNotifySpy = spy(testDevice, 'eventNotify');
+
+    eventCallback!(1);
+
+    expect(eventNotifySpy).calledOnceWith(new Event(testDevice, 'test', 1));
+
+    expect(mockConsumedThing.subscribeEvent).calledOnceWith('test');
+    testDevice.destroy();
+  });
+});
