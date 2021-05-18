a94ab02e2c602f3680425f6646dddaa9e4dd9bac
fatadel <fatadel@gmail.com>
2021-05-18 20:12:52 +0300

    feat: add test for fetching device from invalid URL

--- test/discovery.spec.ts
+++ test/discovery.spec.ts
@@ -1,15 +1,18 @@
-import { expect } from 'chai';
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
 import { Servient } from '@node-wot/core';
 import { HttpServer } from '@node-wot/binding-http';
 import { direct } from '../src/discovery';
 
+chai.use(chaiAsPromised);
+const expect = chai.expect;
 
 describe('Discovery api tests', () => {
   it('fetch device directly', async () => {
     const servient = new Servient();
     servient.addServer(new HttpServer());
     const WoT = await servient.start();
-    const thing = await WoT.produce({ title: 'non-cached-test-thing' });
+    const thing = await WoT.produce({ title: 'non-cached-test-thing', id: 'test' });
     await thing.expose();
     const fetched = await direct('http://127.0.0.1:8080/non-cached-test-thing');
     // expect(false).to.be.true('fix me');
@@ -19,11 +22,22 @@ describe('Discovery api tests', () => {
     expect(fetched[1]).to.be.false;
     await servient.shutdown();
   });
+  it('invalid url should throw an error', async () => {
+    const servient = new Servient();
+    servient.addServer(new HttpServer());
+    const WoT = await servient.start();
+    const thing = await WoT.produce({ title: 'test-thing', id: 'test' });
+    await thing.expose();
+    // TODO: Currently does NOT work
+    await expect(direct('http://127.0.0.1:8080/invalidurl')).to.be.rejectedWith(Error);
+    // expect(true).to.be.true;
+    await servient.shutdown();
+  });
   it('fetch device from cache (time-dependent)', async () => {
     const servient = new Servient();
     servient.addServer(new HttpServer());
     const WoT = await servient.start();
-    const thing = await WoT.produce({ title: 'cached-test-thing' });
+    const thing = await WoT.produce({ title: 'cached-test-thing', id: 'test' });
     await thing.expose();
     await direct('http://127.0.0.1:8080/cached-test-thing');
     const fetched = await direct('http://127.0.0.1:8080/cached-test-thing');
@@ -37,7 +51,7 @@ describe('Discovery api tests', () => {
     const servient = new Servient();
     servient.addServer(new HttpServer());
     const WoT = await servient.start();
-    const thing = await WoT.produce({ title: 'cached-test-thing' });
+    const thing = await WoT.produce({ title: 'cached-test-thing', id: 'test' });
     await thing.expose();
     await direct('http://127.0.0.1:8080/cached-test-thing');
     // Sleep for 6 seconds before next fetch
