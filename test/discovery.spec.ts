950d4e89486bf48da372299a614ccc760752a496
fatadel <fatadel@gmail.com>
2021-05-26 16:25:57 +0200

    feat: add test for multicast discovery - foundThing event

--- test/discovery.spec.ts
+++ test/discovery.spec.ts
@@ -2,7 +2,8 @@ import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import { Servient } from '@node-wot/core';
 import { HttpServer } from '@node-wot/binding-http';
-import { direct } from '../src/discovery';
+import { direct, multicast } from '../src/discovery';
+import { Advertisement, ServiceType } from 'dnssd';
 
 chai.use(chaiAsPromised);
 const expect = chai.expect;
@@ -63,5 +64,33 @@ describe('Discovery api tests', () => {
     expect(fetched[1]).to.be.true;
     await servient.shutdown();
   }).timeout(7000);
+  it('multicast thing discovery - foundThing', (done) => {
+    (async () => {
+      // Start the WoT servient
+      const servient = new Servient();
+      servient.addServer(new HttpServer());
+      const WoT = await servient.start();
+      // Produce and expose the test Thing
+      const thing = await WoT.produce({ title: 'multicast-test-thing', id: 'test' });
+      await thing.expose();
+      // Get an instance of MDNSDiscovery
+      const discovery = multicast();
+      // Add a handler to check for the right event
+      discovery.on('foundThing', (service) => {
+        // Clean up everything
+        servient.shutdown();
+        discovery.stop();
+        ad.stop();
+        // Compare TDs
+        expect(thing.getThingDescription()).to.deep.eq(service.td[0]);
+        // Since `expect` is inside callback, we need `done` in the end
+        done();
+      });
+      discovery.start();
+      // Create and start the service to advertise the Thing
+      const ad = new Advertisement(new ServiceType('_wot.tcp'), 8080, { txt: { path: '/multicast-test-thing' } });
+      ad.start();
+    })();
+  }).timeout(5000);
 });
 
