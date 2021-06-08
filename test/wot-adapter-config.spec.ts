0a652fc76438b193cfe22819bde977457aca9d2b
Lorenzo Trivelli <trivo78@gmail.com>
2021-06-08 05:41:58 +0200

    addedd more test

--- test/wot-adapter-config.spec.ts
+++ test/wot-adapter-config.spec.ts
@@ -1,9 +1,10 @@
+/* eslint-disable max-len */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import chai, { expect } from 'chai';
 import WoTDevice from '../src/wot-device';
 import { stubInterface } from 'ts-sinon';
 import { ConsumedThing } from '@node-wot/core';
-import { WebThingEndpoint, WoTAdapterConfig } from '../src/wot-adapter-config';
+import { WebThingEndpoint, WoTAdapterConfig, NoSecurityData, BasicSecurityData } from '../src/wot-adapter-config';
 import sinon, { fake, SinonStub, spy } from 'sinon';
 import { AddonManagerProxy, Event } from 'gateway-addon';
 import { verbose, Database as SQLiteDatabase } from 'sqlite3';
@@ -47,7 +48,12 @@ describe('WoT Config data', () => {
   const url1 = 'http://pippo.txt';
   const url2 = 'http://pluto.txt';
   const url3 = 'http://nourl.txt';
+  const url4 = 'http://noauth.txt';
+  const url5 = 'http://mdauth.txt';
 
+
+  const noauth = { schema: 'nosec' } as NoSecurityData;
+  const bauth = { schema: 'basic', user: 'tex', password: 'willer' } as BasicSecurityData;
 
   it('Should save and load', async () => {
     {
@@ -55,6 +61,8 @@ describe('WoT Config data', () => {
 
       w.add({ url: url1 });
       w.add({ url: url2 });
+      w.add({ url: url4, authentication: noauth });
+      w.add({ url: url5, authentication: bauth });
       console.log(JSON.stringify(w));
 
       await w.save();
@@ -65,8 +73,12 @@ describe('WoT Config data', () => {
       expect(w.containsUrl(url1)).be.true;
       expect(w.containsUrl(url2)).be.true;
       expect(w.containsUrl(url3)).be.false;
+      expect(w.containsUrl(url4)).be.true;
+      expect(w.containsUrl(url5)).be.true;
 
       expect(w.getUrlData(url1)).be.undefined;
+      expect(w.getUrlData(url4) == noauth);
+      expect(w.getUrlData(url5) == bauth);
     }
   });
 });
